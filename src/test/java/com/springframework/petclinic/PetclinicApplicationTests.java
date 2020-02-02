e14cd11ca73e092a86879116d9bfc6527fc52640
alishatergholi <shatergholi6@gmail.com>
2020-02-02 12:07:13 +0100

    Initial Project Add

---
+++ src/test/java/com/springframework/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
