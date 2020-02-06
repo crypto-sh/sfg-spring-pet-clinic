f6955189984ff85d2e5f46a34613d446a731d549
alishatergholi <shatergholi6@gmail.com>
2020-02-06 11:12:53 +0100

    Add Pet Clinic Index Page and Controller. closes #7.

--- pet-clinic-web/src/main/java/com/springframework/web/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/web/PetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetclinicApplication {
+public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetclinicApplication.class, args);
+        SpringApplication.run(PetClinicApplication.class, args);
     }
 
 }
