9c4e601a1e39adabc7c53a046e847b2c18539e58
a.shatergholi <a.shatergholi@planradar.com>
2023-11-22 17:38:25 +0100

    add specialties service map impl Closes #31

---
+++ pet-clinic-data/src/main/java/com/springframework/Services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.springframework.Services;
+
+import com.springframework.model.Specialty;
+
+public interface SpecialtiesService  extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/Services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package com.springframework.Services.map;
+
+import com.springframework.Services.SpecialtiesService;
+import com.springframework.model.Specialty;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+}
