3c93db9719b53dc25584316ddee8e1be1e968c44
Ali Shatergholi <a.shatergholi@planradar.com>
2023-12-27 15:12:53 +0100

    Create Spring Data JPA Service for Owners. Closes #40

---
+++ pet-clinic-data/src/main/java/com/springframework/Services/springDataJpa/OwnerSDJpaService.java
@@ -0,0 +1,59 @@
+package com.springframework.Services.springDataJpa;
+
+import com.springframework.Services.OwnerService;
+import com.springframework.Services.PetTypeService;
+import com.springframework.model.Owner;
+import com.springframework.repositories.OwnerRepository;
+import com.springframework.repositories.PetRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeService petTypeService;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeService petTypeService) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+}
--- pet-clinic-data/src/main/java/com/springframework/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/repositories/OwnerRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
+    Owner findByLastName(String lastName);
+
 }
