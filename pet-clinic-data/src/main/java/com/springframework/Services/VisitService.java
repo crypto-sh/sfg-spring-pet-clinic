5f74a323782f399101331e043354070b64c98a06
Ali Shatergholi <a.shatergholi@planradar.com>
2023-12-27 15:44:46 +0100

    Create visit map service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/springframework/Services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.Services;
+
+import com.springframework.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/Services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.springframework.Services.map;
+
+import com.springframework.Services.VisitService;
+import com.springframework.model.Visit;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
