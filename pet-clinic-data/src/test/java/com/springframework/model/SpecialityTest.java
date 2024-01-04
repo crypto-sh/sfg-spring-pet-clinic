0baa0fc51b078bdb37a0a82778749e09f067eaaa
Ali Shatergholi <a.shatergholi@planradar.com>
2024-01-04 12:35:32 +0100

    Upgrade to Unit5. Closes #9

---
+++ pet-clinic-data/src/test/java/com/springframework/model/SpecialityTest.java
@@ -0,0 +1,12 @@
+package com.springframework.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecialityTest {
+    @Test
+    void test() {
+
+    }
+}
